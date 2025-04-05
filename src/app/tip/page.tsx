4e584282bddbbc58182b3c80b17ab7f65f79c621
PJijin <pjijin1@gmail.com>
2025-04-06 02:46:15 +0800

    fix: updated tip page

---
+++ src/app/tip/page.tsx
@@ -0,0 +1,11 @@
+import TipWidget from "../widgets/TipWidget";
+
+const Page = () => {
+  return (
+    <div className={`w-full max-w-md  rounded-2xl p-4 shadow-xl mx-auto`}>
+      <TipWidget />
+    </div>
+  );
+};
+
+export default Page;
