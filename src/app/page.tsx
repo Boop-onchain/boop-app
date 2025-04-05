5f3a6c21aefe3273816574ffcb1aa11b932d588a
PJijin <pjijin1@gmail.com>
2025-04-05 10:47:40 +0800

    fix: updated logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid bg-[#1A1A1A] grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        BOOP
+        <img src="/logo.svg" alt="Boop" />
       </main>
     </div>
   );
